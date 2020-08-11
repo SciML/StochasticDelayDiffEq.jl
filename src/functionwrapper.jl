236a0fcce8e592d87475a84bbb58a381d679f948
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-08-11 08:39:50 -0400

    fix function wrapping

--- src/functionwrapper.jl
+++ src/functionwrapper.jl
@@ -8,7 +8,7 @@ end
 (g::SDEDiffusionTermWrapper{true})(du, u, p, t) = g.g(du, u, g.h, p, t)
 (g::SDEDiffusionTermWrapper{false})(u, p, t) = g.g(u, g.h, p, t)
 
-struct SDEFunctionWrapper{iip,F,G,H,TMM,Ta,Tt,TJ,JP,TW,TWt,TPJ,S,TCV} <: DiffEqBase.AbstractRODEFunction{iip}
+struct SDEFunctionWrapper{iip,F,G,H,TMM,Ta,Tt,TJ,JVP,VJP,JP,SP,TW,TWt,TPJ,GG,S,TCV} <: DiffEqBase.AbstractRODEFunction{iip}
   f::F
   g::G
   h::H
@@ -16,10 +16,14 @@ struct SDEFunctionWrapper{iip,F,G,H,TMM,Ta,Tt,TJ,JP,TW,TWt,TPJ,S,TCV} <: DiffEqB
   analytic::Ta
   tgrad::Tt
   jac::TJ
+  jvp::JVP
+  vjp::VJP
   jac_prototype::JP
+  sparsity::SP
   Wfact::TW
   Wfact_t::TWt
   paramjac::TPJ
+  ggprime::GG
   syms::S
   colorvec::TCV
 end
@@ -30,7 +34,7 @@ end
 
 function wrap_functions_and_history(f::SDDEFunction, g, h)
   gwh = SDEDiffusionTermWrapper{isinplace(g,5),typeof(g),typeof(h)}(g,h)
-  
+
   if f.jac === nothing
     jac = nothing
   else
@@ -46,10 +50,12 @@ function wrap_functions_and_history(f::SDDEFunction, g, h)
   end
 
   SDEFunctionWrapper{isinplace(f),typeof(f.f),typeof(gwh),typeof(h),typeof(f.mass_matrix),
-                     typeof(f.analytic),typeof(f.tgrad),typeof(jac),
-                     typeof(f.jac_prototype),typeof(f.Wfact),typeof(f.Wfact_t),
-                     typeof(f.paramjac),typeof(f.syms),typeof(f.colorvec)}(
+                     typeof(f.analytic),typeof(f.tgrad),typeof(jac),typeof(f.jvp),
+                     typeof(f.vjp),typeof(f.jac_prototype),typeof(f.sparsity),
+                     typeof(f.Wfact),typeof(f.Wfact_t),typeof(f.paramjac),
+                     typeof(f.ggprime),typeof(f.syms),typeof(f.colorvec)}(
                        f.f, gwh, h, f.mass_matrix, f.analytic, f.tgrad, jac,
-                       f.jac_prototype, f.Wfact, f.Wfact_t, f.paramjac, f.syms,
-                       f.colorvec), gwh
+                       f.jvp, f.vjp,
+                       f.jac_prototype, f.sparsity, f.Wfact, f.Wfact_t,
+                       f.paramjac, f.ggprime, f.syms, f.colorvec), gwh
 end
