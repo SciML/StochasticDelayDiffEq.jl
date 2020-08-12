49c0a77c38cfd5e23d73d62fcfc7906d39970151
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-08-11 20:31:03 -0400

    add a bunch of automated convergence testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using SafeTestsets
 
-@safetestset "SDDEProblem, solve" begin
-    include("test_prob_sol.jl")
-end
+@safetestset "SDDEProblem, solve" begin include("test_prob_sol.jl") end
+@safetestset "Analyticless Convergence Tests" begin include("analyticless_convergence_tests.jl") end
