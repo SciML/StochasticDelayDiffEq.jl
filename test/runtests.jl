f5d3c7f91e41b31a37d5df9e436d15eebe68db43
David Widmann <dev+git@devmotion.de>
2021-01-25 21:34:34 +0100

    Fix #34

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,4 @@ using SafeTestsets
 
 @safetestset "SDDEProblem, solve" begin include("test_prob_sol.jl") end
 @safetestset "Analyticless Convergence Tests" begin include("analyticless_convergence_tests.jl") end
+@safetestset "Event handling" begin include("events.jl") end
