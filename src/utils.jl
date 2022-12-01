7b78330d1b21003d96021ae1f111e05e14440eef
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-12-01 11:24:06 +0100

    namespace

--- src/utils.jl
+++ src/utils.jl
@@ -241,8 +241,8 @@ function initialize_solution!(integrator::SDDEIntegrator)
     nothing
 end
 
-function OrdinaryDiffEq.nlsolve_f(integrator::SDDEIntegrator)
-    OrdinaryDiffEq.nlsolve_f(integrator.f, unwrap_alg(integrator, true))
+function StochasticDiffEq.OrdinaryDiffEq.nlsolve_f(integrator::SDDEIntegrator)
+    StochasticDiffEq.OrdinaryDiffEq.nlsolve_f(integrator.f, unwrap_alg(integrator, true))
 end
 
 function unwrap_alg(integrator::SDDEIntegrator, is_stiff)
